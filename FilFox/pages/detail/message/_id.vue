210ca401ea88b0c9a2abcae81a3b89add226c524
m-stephen <truestyle2005@163.com>
2020-06-22 19:10:36 +0800

    fix ui bug

--- FilFox/pages/detail/message/_id.vue
+++ FilFox/pages/detail/message/_id.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mx-auto flex flex-col">
       <div class="flex flex-grow-0 mt-6 font-medium"> {{ $t('detail.message.title') }} </div>
-      <div class="flex flex-col flex-grow rounded-md my-2 bg-white">
+      <div class="flex flex-col flex-grow rounded-md my-4 bg-white">
         <dl class="flex flex-row mt-4 my-2 items-center">
           <dt class="min-w-1/8 pl-8 text-gray-600 px-2"> {{ $t('detail.message.headers.cid') }} </dt>
           <dd class="flex mr-4">  <BlockLink :id="id" plain /> </dd>
@@ -91,7 +91,7 @@
             <dd class="flex mr-4">  {{ message.error }} </dd>
           </dl>
 
-          <dl class="flex flex-row items-center my-2">
+          <dl class="flex flex-row items-start my-2">
             <dt class="min-w-1/8 pl-8 text-gray-600 px-2"> {{ $t('detail.message.headers.params') }} </dt>
             <dd class="flex break-all mr-4">  {{ message.params ? message.params : $t('detail.message.null') }} </dd>
           </dl>
