0481860d91bfefef2a79ccdc5cc9980093bbafb3
m-stephen <truestyle2005@163.com>
2020-07-03 14:45:41 +0800

    miner ranks for mobile

--- FilFox/components/home/rank-index.vue
+++ FilFox/components/home/rank-index.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="lg:flex lg:m-auto"> 
-        <img v-if="index === 1" src="~/assets/img/home/first.png" alt="1" class="w-4 h-auto ml-1 lg:m-auto lg:w-6">
+        <img v-if="index === 1" src="~/assets/img/home/first.png" alt="1" class="w-4 h-auto ml-1  lg:m-auto lg:w-6">
         <img v-if="index === 2" src="~/assets/img/home/second.png" alt="1" class="w-4 h-auto ml-1 lg:m-auto lg:w-6">
-        <img v-if="index === 3" src="~/assets/img/home/third.png" alt="1" class="w-4 h-auto ml-1 lg:m-auto lg:w-6">
+        <img v-if="index === 3" src="~/assets/img/home/third.png" alt="1" class="w-4 h-auto ml-1  lg:m-auto lg:w-6">
         <p v-if="index > 3" class="flex ml-2 lg:text-center lg:m-auto"> {{ index }} </p>
     </div>
 </template>
