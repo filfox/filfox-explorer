903526503132b9f19bba186458b014bacdbb4e90
m-stephen <truestyle2005@163.com>
2020-07-03 14:59:23 +0800

    improve ui

--- components/home/rank-index.vue
+++ components/home/rank-index.vue
@@ -3,7 +3,7 @@
         <img v-if="index === 1" src="~/assets/img/home/first.png" alt="1" class="w-4 h-auto ml-1  lg:m-auto lg:w-6">
         <img v-if="index === 2" src="~/assets/img/home/second.png" alt="1" class="w-4 h-auto ml-1 lg:m-auto lg:w-6">
         <img v-if="index === 3" src="~/assets/img/home/third.png" alt="1" class="w-4 h-auto ml-1  lg:m-auto lg:w-6">
-        <p v-if="index > 3" class="flex ml-2 lg:text-center lg:m-auto"> {{ index }} </p>
+        <p v-if="index > 3" class="flex ml-1 rounded-full px-1 bg-background text-main lg:bg-white lg:text-black lg:text-center lg:m-auto"> {{ index }} </p>
     </div>
 </template>
 
