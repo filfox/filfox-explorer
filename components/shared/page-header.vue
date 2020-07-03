04f2fc7cc366bc204e898395e1288df56a6c0272
m-stephen <truestyle2005@163.com>
2020-07-03 15:59:46 +0800

    fix bug

--- components/shared/page-header.vue
+++ components/shared/page-header.vue
@@ -1,8 +1,8 @@
 <template>
-    <nav class="mx-auto bg-main h-28 w-full ">
+    <nav class="mx-auto bg-main h-28 w-full">
        <div class="flex h-16 container mx-0 lg:mx-auto items-center pt-2">
            <nuxt-link :to="localePath('/')">
-               <img src="~/assets/img/home/logo.svg" alt="filFox.io" class="cursor-pointer h-6 ml-4 lg:ml-0">
+               <img src="~/assets/img/home/logo.svg" alt="filFox.io" class="cursor-pointer h-6 lg:h-8 ml-4 lg:ml-0">
            </nuxt-link>
        </div>
        <navigationBar/>
