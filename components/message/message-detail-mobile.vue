10720e8fb8558636f93e86617e56f154189be9c4
m-stephen <truestyle2005@163.com>
2020-07-06 11:34:11 +0800

    message detail

--- components/message/message-detail-mobile.vue
+++ components/message/message-detail-mobile.vue
@@ -3,108 +3,97 @@
 
       <div class="flex flex-grow-0 font-medium text-sm pl-4 py-3 border-b border-background"> {{ $t('detail.message.title') }} </div>
       
-      <div class="flex flex-row justify-between items-center text-xs">
-          <p></p>
+      <div class="flex flex-row justify-between items-center text-xs mx-4 mt-2">
+          <p class="flex w-1/4">{{ $t('detail.message.headers.cid') }}</p>
+          <BlockLink :id="id" class="flex w-3/4" />
       </div>
 
+      <div class="flex flex-row justify-between items-center text-xs mx-4 mt-2">
+          <p class="flex w-1/4">{{ $t('detail.message.headers.height') }}</p>
+          <TipsetLink :id="message.height" class=" text-main flex w-3/4" />
+      </div>
 
-      <div class="flex flex-col flex-grow rounded-md my-4 bg-white">
-        <dl class="flex flex-row mt-4 my-2 items-center">
-          <dt class="min-w-1/8 pl-8 text-gray-600 px-2"> {{ $t('detail.message.headers.cid') }} </dt>
-          <dd class="flex mr-4">  <BlockLink :id="id" plain /> </dd>
-        </dl>
+      <div class="flex flex-row justify-between items-center text-xs mx-4 mt-2">
+          <p class="flex w-1/4">{{ $t('detail.message.headers.time') }}</p>
+          <p class="flex w-3/4"> {{ message.timestamp | timestamp }} </p>
+      </div>
 
-        <dl class="flex flex-row my-2 items-center">
-          <dt class="min-w-1/8 pl-8 text-gray-600 px-2"> {{ $t('detail.message.headers.height') }} </dt>
-          <dd class="flex mr-4">  <TipsetLink :id="message.height" class=" text-main" /> </dd>
-        </dl>
-
-        <dl class="flex flex-row my-2 items-center">
-          <dt class="min-w-1/8 pl-8 text-gray-600 px-2"> {{ $t('detail.message.headers.time') }} </dt>
-          <dd class="flex mr-4">  {{ message.timestamp | timestamp }} </dd>
-        </dl>
-
-        <dl class="flex flex-row items-center my-2">
-          <dt class="min-w-1/8 pl-8 text-gray-600 px-2 items-center"> {{ $t('detail.message.headers.inBlocks') }} </dt>
-          <dd class="flex flex-col mr-4">  
+      <div class="flex flex-row justify-between items-center text-xs mx-4 mt-2">
+          <p class="flex w-1/4">{{ $t('detail.message.headers.inBlocks') }}</p>
+          <div class="flex flex-col w-3/4">
             <p v-for="block in message.blocks" :key="block" class="items-center flex text-main">
-              <BlockLink :id="block" />
+                <BlockLink :id="block" />
             </p>
-          </dd>
-        </dl>
+          </div>
+      </div>
 
-        <dl class="flex flex-row my-2 items-center">
-          <dt class="min-w-1/8 pl-8 text-gray-600 px-2"> {{ $t('detail.message.headers.version') }} </dt>
-          <dd class="flex mr-4">  {{ message.version }} </dd>
-        </dl>
+      <div class="flex flex-row justify-between items-center text-xs mx-4 pt-2">
+          <p class="flex w-1/4">{{ $t('detail.message.headers.version') }}</p>
+          <p class="flex w-3/4"> {{ message.version }} </p>
+      </div>
 
-        <dl class="flex flex-row my-2 items-center">
-          <dt class="min-w-1/8 pl-8 text-gray-600 px-2"> {{ $t('detail.message.headers.from') }} </dt>
-          <dd class="flex mr-4"> 
-            <AddressLink :id="message.from" class="text-main"/> 
-            <div v-if="message.fromTag" class="flex ml-2 text-xs rounded-full px-2 border border-gray-400 items-center"> {{ message.fromTag[$i18n.locale] }} </div>
-          </dd>
-        </dl>
+      <div class="flex flex-row justify-between items-center text-xs mx-4 pt-2">
+          <p class="flex w-1/4">{{ $t('detail.message.headers.from') }} </p>
+          <AddressLink :id="message.from" class="text-main w-3/4"/>
+      </div>
 
-        <dl class="flex flex-row my-2 items-center">
-          <dt class="min-w-1/8 pl-8 text-gray-600 px-2"> {{ $t('detail.message.headers.to') }} </dt>
-          <dd class="flex mr-4">  
-            <AddressLink :id="message.to" class="text-main"/> 
-            <div v-if="message.toTag" class="flex ml-2 text-xs rounded-full px-2 border border-gray-400 items-center"> {{ message.toTag[$i18n.locale] }} </div>
-          </dd>
-        </dl>
+      <div class="flex flex-row justify-between items-center text-xs mx-4 pt-2">
+          <p class="flex w-1/4">{{ $t('detail.message.headers.to') }}</p>
+          <AddressLink :id="message.to" class="text-main w-3/4"/>
+      </div>
 
-        <dl class="flex flex-row my-2 items-center">
-          <dt class="min-w-1/8 pl-8 text-gray-600 px-2"> {{ $t('detail.message.headers.nonce') }} </dt>
-          <dd class="flex mr-4">  {{ message.nonce }} </dd>
-        </dl>
+      <div class="flex flex-row justify-between items-center text-xs mx-4 pt-2">
+          <p class="flex w-1/4">{{ $t('detail.message.headers.nonce') }}</p>
+          <p class="flex w-3/4"> {{ message.nonce }} </p>
+      </div>
 
-        <dl class="flex flex-row my-2 items-center">
-          <dt class="min-w-1/8 pl-8 text-gray-600 px-2"> {{ $t('detail.message.headers.method') }} </dt>
-          <dd class="flex mr-4">  {{ message.method }} </dd>
-        </dl>
+      <div class="flex flex-row justify-between items-center text-xs mx-4 pt-2">
+          <p class="flex w-1/4">{{ $t('detail.message.headers.method') }}</p>
+          <p class="flex w-3/4"> {{ message.method }} </p>
+      </div>
 
-        <dl class="flex flex-row my-2 items-center">
-          <dt class="min-w-1/8 pl-8 text-gray-600 px-2"> {{ $t('detail.message.headers.value') }} </dt>
-          <dd class="flex mr-4">  {{ message.value | filecoin() }} </dd>
-        </dl>
+      <div class="flex flex-row justify-between items-center text-xs mx-4 pt-2">
+          <p class="flex w-1/4">{{ $t('detail.message.headers.value') }}</p>
+          <p class="flex w-3/4"> {{ message.value | filecoin() }} </p>
+      </div>
 
-        <dl class="flex flex-row my-2 items-center">
-          <dt class="min-w-1/8 pl-8 text-gray-600 px-2"> {{ $t('detail.message.headers.gasPrice') }} </dt>
-          <dd class="flex mr-4">  {{ message.gasPrice | filecoin() }} </dd>
-        </dl>
+      <div class="flex flex-row justify-between items-center text-xs mx-4 pt-2">
+          <p class="flex w-1/4">{{ $t('detail.message.headers.gasPrice') }}</p>
+          <p class="flex w-3/4"> {{ message.gasPrice | filecoin() }} </p>
+      </div>
 
-        <dl class="flex flex-row my-2 items-center">
-          <dt class="min-w-1/8 pl-8 text-gray-600 px-2"> {{ $t('detail.message.headers.gasLimit') }} </dt>
-          <dd class="flex mr-4">  {{ message.gasLimit | locale }} </dd>
-        </dl>
+      <div class="flex flex-row justify-between items-center text-xs mx-4 pt-2">
+          <p class="flex w-1/4">{{ $t('detail.message.headers.gasLimit') }}</p>
+          <p class="flex w-3/4"> {{ message.gasLimit | locale }} </p>
+      </div>
 
-        <template v-if="message.receipt">
-          <dl class="flex flex-row my-2 items-center">
-            <dt class="min-w-1/8 pl-8 text-gray-600 px-2"> {{ $t('detail.message.headers.gasUsed') }} </dt>
-            <dd class="flex mr-4">  {{ message.receipt.gasUsed | locale  }} </dd>
-          </dl>
+      <template v-if="message.receipt">
 
-          <dl class="flex flex-row my-2 items-center">
-            <dt class="min-w-1/8 pl-8 text-gray-600 px-2"> {{ $t('detail.message.headers.exitCode') }} </dt>
-            <dd class="flex mr-4">  {{ message.receipt.exitCode | exit-code  }} </dd>
-          </dl>
+        <div class="flex flex-row justify-between items-center text-xs mx-4 pt-2">
+          <p class="flex w-1/4"> {{ $t('detail.message.headers.gasUsed') }} </p>
+          <p class="flex w-3/4"> {{ message.receipt.gasUsed | locale  }} </p>
+      </div>
 
-          <dl v-if="message.receipt.exitCode === 0" class="flex flex-row my-2 items-center">
-            <dt class="min-w-1/8 pl-8 text-gray-600 px-2"> {{ $t('detail.message.headers.return') }} </dt>
-            <dd class="flex mr-4">  {{ message.receipt.return ? message.receipt.return : $t('detail.message.null')  }} </dd>
-          </dl>
+        <div class="flex flex-row justify-between items-center text-xs mx-4 pt-2">
+            <p class="flex w-1/4">{{ $t('detail.message.headers.exitCode') }}</p>
+            <p class="flex w-3/4"> {{ message.receipt.exitCode | exit-code  }} </p>
+        </div>
 
-          <dl v-else class="flex flex-row my-2 items-center">
-            <dt class="min-w-1/8 pl-8 text-gray-600 px-2"> {{ $t('detail.message.headers.error') }} </dt>
-            <dd class="flex mr-4">  {{ message.error }} </dd>
-          </dl>
+        <div class="flex flex-row justify-between items-center text-xs mx-4 pt-2" v-if="message.receipt.exitCode === 0">
+            <p class="flex w-1/4">{{ $t('detail.message.headers.return') }}</p>
+            <p class="flex w-3/4"> {{ message.receipt.return ? message.receipt.return : $t('detail.message.null')  }} </p>
+        </div>
 
-          <dl class="flex flex-row items-start my-2">
-            <dt class="min-w-1/8 pl-8 text-gray-600 px-2"> {{ $t('detail.message.headers.params') }} </dt>
-            <dd class="flex break-all mr-4">  {{ message.params ? message.params : $t('detail.message.null') }} </dd>
-          </dl>
-        </template>
+        <div class="flex flex-row justify-between items-center text-xs mx-4 pt-2" v-else>
+            <p class="flex w-1/4">{{ $t('detail.message.headers.error') }}</p>
+            <p class="flex w-3/4"> {{ message.error }} </p>
+        </div>
+
+        <div class="flex flex-row justify-between items-start text-xs mx-4 pt-2">
+            <p class="flex w-1/4">{{ $t('detail.message.headers.params') }}</p>
+            <p class="flex break-all w-3/4"> {{ message.params ? message.params : $t('detail.message.null') }} </p>
+        </div>
+      </template>
       
       </div>
   </div>
