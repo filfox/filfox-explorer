0dbf6cbc2e06773870fa560c913a25eaecd0cfa1
m-stephen <truestyle2005@163.com>
2020-07-08 11:58:23 +0800

    load chart if needed

--- plugins/chart.client.js
+++ plugins/chart.client.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
-import VCharts from 'v-charts'
+import VeLine from 'v-charts/lib/line.common'
+import VeHistogram from 'v-charts/lib/histogram.common'
+import VePie from 'v-charts/lib/pie.common'
+import VeRing from 'v-charts/lib/ring.common'
 
-Vue.use(VCharts)
+Vue.component(VeLine.name, VeLine)
+Vue.component(VeHistogram.name, VeHistogram)
+Vue.component(VePie.name, VePie)
+Vue.component(VeRing.name, VeRing)
