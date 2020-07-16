2e43ef3126151b3813554122a18ec8752635fa81
m-stephen <truestyle2005@163.com>
2020-07-16 21:26:13 +0800

    init merchant page for PC

---
+++ pages/merchant/_id.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="flex container mx-auto">
+        <MerchantDetail :merchant="merchantInfo"/>
+    </div>
+
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios, params, query, error }) {
+    const id = params.id;
+    try {
+      const merchantInfo = await $axios.$get(`/merchant/${id}`);
+      return { merchantInfo };
+    } catch (err) {
+      if (err?.response) {
+        if (err.response.code === 404) {
+          error({ code: 404, message: `Merchant ${id} not found` });
+        } else {
+          error({
+            code: err.response.status,
+            message: err.response.statusText
+          });
+        }
+      } else {
+        error({ code: 500, message: err.toString() });
+      }
+    }
+  },
+  data() {
+    return {
+      merchantInfo: {}
+    }
+  },
+  head() {
+    return {
+      title: `${this.merchantInfo.name}`
+    }
+  }
+}
+</script>
