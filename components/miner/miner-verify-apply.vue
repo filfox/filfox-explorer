92233173c59a8939a52e9d07bc39550898e9605f
Xuan Yan <xuanyan@xuanyan.ws>
2020-07-27 02:11:37 +0800

    add original message to verify modal

--- components/miner/miner-verify-apply.vue
+++ components/miner/miner-verify-apply.vue
@@ -1,69 +1,77 @@
 <template>
     <el-dialog :title="$t('tag.sign')" :visible.sync="dialogVisible" width="50%" center >
         <div v-loading="loading" v-if="loading" class="h-20"> </div>
-        <div class="flex flex-col" v-if="!loading" v-loading="submitLoading">
+        <div class="flex flex-col" v-else v-loading="submitLoading">
             <div class=" font-medium"> {{ $t('tag.description',{power: message.data ? getAdjPower(message.data.minPower,2) : '0 B' }) }} </div>
 
             <div class="flex flex-row items-center mt-4">
-                <div class="w-1/6  flex"> {{ $t('tag.owner')}} <p class="ml-1 text-red-600"> * </p>  
+                <div class="w-1/6 flex">
+                    {{ $t('tag.owner')}}
+                    <p class="ml-1 text-red-600">*</p>
                 </div>
                 <p class="flex w-5/6 pl-2" v-if="message.data"> {{ message.data.addressToSign ?  message.data.addressToSign : ""}} </p>
             </div>
 
             <div class="flex flex-row items-center mt-4">
-                <div class="w-1/6 flex"> {{ $t('tag.zh_sname') }} <p class="ml-1 text-red-600"> * </p>  
-                </div>
-                <el-input size="small" :placeholder="$t('tag.snamePlaceholder')" v-model="zh" class="flex w-5/6 pl-2">
-                </el-input>
-            </div>
-
-            <div class="flex flex-row items-center mt-4">
-                <div class="w-1/6 flex"> {{ $t('tag.en_sname') }} <p class="ml-1 text-red-600"> * </p>  
+                <div class="w-1/6 flex">
+                    {{ $t('tag.en_sname') }}
+                    <p class="ml-1 text-red-600">*</p>
                 </div>
                 <el-input size="small" :placeholder="$t('tag.snamePlaceholder')" v-model="en" class="flex w-5/6 pl-2">
                 </el-input>
             </div>
 
             <div class="flex flex-row items-center mt-4">
-                <div class="w-1/6 flex"> {{ $t('tag.zh_name') }}
-                </div>
-                <el-input size="small" :placeholder="$t('tag.namePlaceholder')" v-model="zh_full" class="flex w-5/6 pl-2">
-                </el-input>
-            </div>
-
-            <div class="flex flex-row items-center mt-4">
-                <div class="w-1/6 flex"> {{ $t('tag.en_name') }} 
-                </div>
+                <div class="w-1/6 flex">{{ $t('tag.en_name') }}</div>
                 <el-input size="small" :placeholder="$t('tag.namePlaceholder')" v-model="en_full" class="flex w-5/6 pl-2">
                 </el-input>
             </div>
 
             <div class="flex flex-row items-center mt-4">
-                <div class="w-1/6  flex"> {{ $t('tag.contact') }}  <p class="ml-1 text-red-600"> * </p>
+                <div class="w-1/6 flex">{{ $t('tag.zh_sname') }}</div>
+                <el-input size="small" :placeholder="$t('tag.snamePlaceholder')" v-model="zh" class="flex w-5/6 pl-2">
+                </el-input>
+            </div>
+
+            <div class="flex flex-row items-center mt-4">
+                <div class="w-1/6 flex">{{ $t('tag.zh_name') }}</div>
+                <el-input size="small" :placeholder="$t('tag.namePlaceholder')" v-model="zh_full" class="flex w-5/6 pl-2">
+                </el-input>
+            </div>
+
+            <div class="flex flex-row items-center mt-4">
+                <div class="w-1/6 flex">
+                    {{ $t('tag.contact') }}
+                    <p class="ml-1 text-red-600">*</p>
                 </div>
                 <el-input size="small" :placeholder="$t('tag.contactPlaceHolder')" v-model="contact" class="flex w-5/6 pl-2">
                 </el-input>
             </div>
 
-            <div class="flex flex-row items-center mt-4">
-                <div class="w-1/6 flex"> {{ $t('tag.message') }} 
+            <template v-if="message.data">
+                <div class="flex flex-row items-center mt-4">
+                    <div class="w-1/6 flex"></div>
+                    <div class="w-5/6 flex items-center justify-between pl-2">
+                        <highlight class="flex overflow-hidden">{{ message.data.message }}</highlight>
+                    </div>
                 </div>
-                <div class="w-5/6 flex items-center justify-between pl-2">
-                    <highlight class="flex overflow-hidden mr-3">
-                        <template v-slot v-if="message.data"> {{ message.data.command.lotus }} </template>
-                    </highlight>
-                    <el-button icon="el-icon-document-copy" size="small" @click="didCopyClicked">
-                    </el-button>
+                <div class="flex flex-row items-center mt-4">
+                    <div class="w-1/6 flex">{{ $t('tag.message') }}</div>
+                    <div class="w-5/6 flex items-center justify-between pl-2">
+                        <highlight class="flex overflow-hidden">{{ message.data.command.lotus }}</highlight>
+                        <el-button icon="el-icon-document-copy" size="small" @click="didCopyClicked"></el-button>
+                    </div>
                 </div>
-            </div>
+            </template>
 
             <div class="flex flex-row items-center mt-4 ">
-                <div class="w-1/6  flex"> {{ $t('tag.signature') }} <p class="ml-1 text-red-600"> * </p>
+                <div class="w-1/6 flex">
+                    {{ $t('tag.signature') }}
+                    <p class="ml-1 text-red-600">*</p>
                 </div>
                 <el-input size="small" :placeholder="$t('tag.signaturePlaceHolder')" class="flex w-5/6 pl-2" v-model="signature">
                 </el-input>
             </div>
-
         </div>
         <span slot="footer" class="dialog-footer">
         <el-button type="primary" @click="confirm"> {{ $t('shared.submit') }} </el-button>
@@ -95,8 +103,8 @@ export default {
             loading: false,
             message: '',
             en:'',
-            zh:'',
             en_full: '',
+            zh:'',
             zh_full: '',
             contact: '',
             signature: '',
@@ -129,10 +137,6 @@ export default {
             this.getSignatureInfo();
         },
         confirm() {
-            if (this.zh.length == 0) {
-                this.$message.error(this.$t('tag.errorInput') + this.$t('tag.zh_sname'))
-                return
-            }
             if (this.en.length == 0) {
                 this.$message.error(this.$t('tag.errorInput') + this.$t('tag.en_sname'))
                 return
@@ -150,11 +154,14 @@ export default {
         submitSignature() {
             this.submitLoading = true
             var params = { address:this.addressInfo, zh:this.zh, en:this.en, contact:this.contact, signature:this.signature};
-            if (this.zh_full.length > 0) {
-                params['zh_full'] = this.zh_full
-            }
             if (this.en_full.length > 0) {
                 params['en_full'] = this.en_full
+            }
+            if (this.zh.length > 0) {
+                params['zh'] = this.zh
+            }
+            if (this.zh_full.length > 0) {
+                params['zh_full'] = this.zh_full
             }
             this.$axios
                 .post(`/address-tag/create`, params
