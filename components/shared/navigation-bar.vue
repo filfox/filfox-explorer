b24227bbfa7ab4d96ee757b0e080d1f8ccc6215f
m-stephen <truestyle2005@163.com>
2020-07-29 15:02:58 +0800

    fix lg nav bug

--- components/shared/navigation-bar.vue
+++ components/shared/navigation-bar.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="container lg:flex px-4 h-12 mx-auto lg:px-0">
-    <nuxt-link :to="localePath('/')" class="flex items-center mr-3 px-2">
+    <nuxt-link :to="localePath('/')" class="flex items-center mr-2 xl:mr-3 px-2">
       <div class="text-background text-sm">
         {{ $t('nav.home.title') }}
       </div>
     </nuxt-link>
 
-    <el-dropdown class="flex items-center mx-3">
+    <el-dropdown class="flex items-center mx-2 xl:mx-3">
       <span class="el-dropdown-link px-2 text-background text-sm">
         {{ $t('nav.blocks.title') }} <i class="el-icon-arrow-down el-icon--right"></i>
       </span>
@@ -23,13 +23,13 @@
       </el-dropdown-menu>
     </el-dropdown>
 
-    <nuxt-link class="flex items-center mx-3 px-2" :to="localePath('/ranks')">
+    <nuxt-link class="flex items-center mx-2 xl:mx-3 px-2" :to="localePath('/ranks')">
       <div class="text-background text-sm">
         {{ $t('nav.ranks.title') }}
       </div>
     </nuxt-link>
 
-    <el-dropdown class="flex items-center mx-3">
+    <el-dropdown class="flex items-center mx-2 xl:mx-3">
       <span class="el-dropdown-link px-2 text-background text-sm">
         {{ $t('nav.charts.title') }} <i class="el-icon-arrow-down el-icon--right"></i>
       </span>
@@ -43,7 +43,7 @@
       </el-dropdown-menu>
     </el-dropdown>
 
-    <el-dropdown class="flex items-center mx-3">
+    <el-dropdown class="flex items-center mx-2 xl:mx-3">
       <span class="el-dropdown-link px-2 text-background text-sm">
         {{ $t('nav.resources.title') }} <i class="el-icon-arrow-down el-icon--right"></i>
       </span>
