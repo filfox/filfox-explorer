100fef59cc1f1708a364e68ba2900b8bbf1ce5b8
Xuan Yan <xuanyan@xuanyan.ws>
2020-08-01 14:01:22 +0800

    fix whitespaces

--- components/home/overview-cell.vue
+++ components/home/overview-cell.vue
@@ -2,7 +2,9 @@
   <div class="flex items-center rounded-sm bg-background">
     <div class="mt-2 lg:mt-8 mb-2 lg:mb-6 lg:mx-auto px-2">
       <div class="text-left lg:text-center text-xs text-gray-500 lg:mx-auto">
-        <span class="align-middle">{{ name }}</span>
+        <span class="align-middle">
+          {{ name }}
+        </span>
         <Tip v-if="tipContent" class="inline-block align-middle" :content="tipContent" />
       </div>
       <div v-if="type === 'normal'" class="text-left lg:text-center text-sm lg:text-2xl items-start lg:mx-auto">
