dba6180ad300bd923947cbb34f5a419b96171f43
m-stephen <truestyle2005@163.com>
2020-08-18 19:03:31 +0800

    update ranks pic for mobile

--- components/social/ranks-share.vue
+++ components/social/ranks-share.vue
@@ -1,12 +1,20 @@
 <template>
-  <div v-if="sharing" class="absolute w-full" :class="{'bg-tableHeaderColor':!loading}" @click="e => didDismissAreaClicked(e)">
-    <div v-if="!loading" class="bg-yellow-500 text-center text-white text-xs py-2">
-      长按图片，保存到手机相册
+  <div>
+    <div v-if="sharing" class="fixed top-0 left-0 h-screen w-screen lg:hidden" :class="{'bg-tableHeaderColor':!loading}" @click="e => didDismissAreaClicked(e)">
+      <div v-if="!loading" class="bg-yellow-500 text-center text-white text-xs py-2">
+        长按图片，保存到手机相册
+      </div>
+      <div v-if="!loading" id="image" class="mx-auto w-3/4 mt-0">
+        <img id="image" :src="url" class="w-full">
+      </div>
     </div>
-    <div v-if="!loading" id="image" class="mx-auto w-3/4 mt-0">
-      <img id="image" :src="url" class="w-full">
-    </div>
-    <div v-if="!loading" style="height:70vh">
+    <div v-if="sharing" class="hidden top-0 left-0 fixed w-screen h-screen lg:flex lg:flex-col lg:justify-center" :class="{'bg-tableHeaderColor':!loading}" @click="e => didDismissAreaClicked(e)">
+      <div v-if="!loading" id="image" class="mx-auto  lg:w-1/4 xl:w-1/5">
+        <img id="image" :src="url" class="w-full">
+      </div>
+      <div v-if="!loading" class="text-white text-center text-sm mt-5">
+        右键复制/保存图片以分享
+      </div>
     </div>
   </div>
 </template>
--- pages/ranks.vue
+++ pages/ranks.vue
@@ -1,73 +1,93 @@
 <template>
-  <div v-loading="sharingLoading" element-loading-text="生成图片中..." element-loading-background="rgba(0, 0, 0, 0.71)" class="bg-white lg:bg-transparent lg:container lg:mx-auto relative">
-    <RanksShare :loading="sharingLoading" :sharing="sharing" :url="sharingImageURL" @didDismissAreaClicked="didDismissAreaClicked" />
-    <div v-if="!sharing" class="lg:hidden">
-      <div class="flex justify-between items-center">
-        <HomeTitle type="minerRanks" />
-        <div v-if="$i18n.locale == 'zh'" class="flex items-center border border-socialTagBg rounded-full px-2 mr-3 text-xs cursor-pointer hover:border-button" @click="didSharedBtnClicked">
-          <img src="~/assets/img/social/share.svg" class="h-2 mr-1">
-          <span> 分享 </span>
-        </div>
-      </div>
-      <div class="flex justify-between">
-        <div class="flex w-full h-10 px-4 items-center relative">
-          <el-tabs v-model="type" class="w-full text-sm">
-            <el-tab-pane :label="$t('home.minerRanks.filters.qualityAdjPower')" name="power" />
-            <el-tab-pane :label="$t('home.minerRanks.filters.blocks')" name="blocks" />
-            <el-tab-pane :label="$t('home.minerRanks.filters.powerDelta')" name="power-growth" />
-          </el-tabs>
-          <DurationSelect v-if="category !== 'power'" v-model="duration" portable class="absolute right-0 bottom-0 mb-4 mr-4" />
-        </div>
-      </div>
+  <div class="bg-white lg:bg-transparent lg:container lg:mx-auto">
+    <div
+      v-if="sharing"
+      v-loading="sharingLoading"
+      element-loading-text="生成图片中..."
+      element-loading-background="rgba(0, 0, 0, 0.71)"
+      class="lg:hidden"
+      style="height:70vh"
+    >
+      <RanksShare :loading="sharingLoading" :sharing="sharing" :url="sharingImageURL" @didDismissAreaClicked="didDismissAreaClicked" />
     </div>
-
-    <div v-if="!sharing" class="mt-4 bg-white rounded-md hidden lg:block">
-      <div class="border-b border-background">
+    <div
+      v-if="sharing"
+      v-loading="sharingLoading"
+      element-loading-text="生成图片中..."
+      element-loading-background="rgba(0, 0, 0, 0)"
+      class="hidden lg:block my-4 bg-transparent"
+      style="height:70vh"
+    >
+      <RanksShare :loading="sharingLoading" :sharing="sharing" :url="sharingImageURL" @didDismissAreaClicked="didDismissAreaClicked" />
+    </div>
+    <div v-if="!sharing">
+      <div class="lg:hidden">
         <div class="flex justify-between items-center">
           <HomeTitle type="minerRanks" />
-          <div v-if="$i18n.locale == 'zh'" class="flex items-center border border-socialTagBg rounded-full px-3 py-1 mr-4 text-xs cursor-pointer hover:border-button">
-            <img src="~/assets/img/social/share.svg" class="h-3 mr-1">
+          <div v-if="$i18n.locale == 'zh'" class="flex items-center border border-socialTagBg rounded-full px-2 mr-3 text-xs cursor-pointer hover:border-button" @click="didSharedBtnClicked">
+            <img src="~/assets/img/social/share.svg" class="h-2 mr-1">
             <span> 分享 </span>
           </div>
         </div>
-        <div class="flex justify-between mb-2">
-          <div class="flex h-12 ml-4 items-center">
-            <el-row>
-              <el-button
-                size="mini"
-                round
-                v-bind="category === 'power' ? {type: 'primary', plain: true, class: 'pointer-events-none'} : {}"
-              >
-                <nuxt-link :to="localePath('/ranks/power')">
-                  {{ $t('home.minerRanks.filters.qualityAdjPower') }}
-                </nuxt-link>
-              </el-button>
-              <el-button
-                size="mini"
-                round
-                v-bind="category === 'blocks' ? {type: 'primary', plain: true, class: 'pointer-events-none'} : {}"
-              >
-                <nuxt-link :to="localePath('/ranks/blocks')">
-                  {{ $t('home.minerRanks.filters.blocks') }}
-                </nuxt-link>
-              </el-button>
-              <el-button
-                size="mini"
-                round
-                v-bind="category === 'power-growth' ? {type: 'primary', plain: true, class: 'pointer-events-none'} : {}"
-              >
-                <nuxt-link :to="localePath('/ranks/power-growth')">
-                  {{ $t('home.minerRanks.filters.powerDelta') }}
-                </nuxt-link>
-              </el-button>
-            </el-row>
+        <div class="flex justify-between">
+          <div class="flex w-full h-10 px-4 items-center relative">
+            <el-tabs v-model="type" class="w-full text-sm">
+              <el-tab-pane :label="$t('home.minerRanks.filters.qualityAdjPower')" name="power" />
+              <el-tab-pane :label="$t('home.minerRanks.filters.blocks')" name="blocks" />
+              <el-tab-pane :label="$t('home.minerRanks.filters.powerDelta')" name="power-growth" />
+            </el-tabs>
+            <DurationSelect v-if="category !== 'power'" v-model="duration" portable class="absolute right-0 bottom-0 mb-4 mr-4" />
           </div>
-          <DurationSelect v-if="category !== 'power'" v-model="duration" class="flex items-center mr-4" />
         </div>
       </div>
-    </div>
 
-    <nuxt-child v-if="!sharing" :duration="duration" />
+      <div class="mt-4 bg-white rounded-md hidden lg:block">
+        <div class="border-b border-background">
+          <div class="flex justify-between items-center">
+            <HomeTitle type="minerRanks" />
+            <div v-if="$i18n.locale == 'zh'" class="flex items-center border border-socialTagBg rounded-full px-3 py-1 mr-4 text-xs cursor-pointer hover:border-button" @click="didSharedBtnClicked">
+              <img src="~/assets/img/social/share.svg" class="h-3 mr-1">
+              <span> 分享 </span>
+            </div>
+          </div>
+          <div class="flex justify-between mb-2">
+            <div class="flex h-12 ml-4 items-center">
+              <el-row>
+                <el-button
+                  size="mini"
+                  round
+                  v-bind="category === 'power' ? {type: 'primary', plain: true, class: 'pointer-events-none'} : {}"
+                >
+                  <nuxt-link :to="localePath('/ranks/power')">
+                    {{ $t('home.minerRanks.filters.qualityAdjPower') }}
+                  </nuxt-link>
+                </el-button>
+                <el-button
+                  size="mini"
+                  round
+                  v-bind="category === 'blocks' ? {type: 'primary', plain: true, class: 'pointer-events-none'} : {}"
+                >
+                  <nuxt-link :to="localePath('/ranks/blocks')">
+                    {{ $t('home.minerRanks.filters.blocks') }}
+                  </nuxt-link>
+                </el-button>
+                <el-button
+                  size="mini"
+                  round
+                  v-bind="category === 'power-growth' ? {type: 'primary', plain: true, class: 'pointer-events-none'} : {}"
+                >
+                  <nuxt-link :to="localePath('/ranks/power-growth')">
+                    {{ $t('home.minerRanks.filters.powerDelta') }}
+                  </nuxt-link>
+                </el-button>
+              </el-row>
+            </div>
+            <DurationSelect v-if="category !== 'power'" v-model="duration" class="flex items-center mr-4" />
+          </div>
+        </div>
+      </div>
+      <nuxt-child :duration="duration" />
+    </div>
   </div>
 </template>
 
