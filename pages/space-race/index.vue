6b68db5e7e4ca88a5648be64d828f5989b185d46
m-stephen <truestyle2005@163.com>
2020-09-02 13:39:37 +0800

    count down

--- pages/space-race/index.vue
+++ pages/space-race/index.vue
@@ -5,40 +5,7 @@
       <img v-else src="~/assets/img/space-race/header-en.png" alt="header" class="w-full mx-auto">
     </div>
 
-    <div class="flex flex-row justify-center items-center mb-2">
-      <div class="mx-2 text-center text-white">
-        <div class="countDown">
-          12
-        </div>
-        <p class="mt-1">
-          {{ $t('shared.time.day') }}
-        </p>
-      </div>
-      <div class="mx-2 text-center text-white">
-        <div class="countDown">
-          01
-        </div>
-        <p class="mt-1">
-          {{ $t('shared.time.hour') }}
-        </p>
-      </div>
-      <div class="mx-2 text-center text-white">
-        <div class="countDown">
-          22
-        </div>
-        <p class="mt-1">
-          {{ $t('shared.time.min') }}
-        </p>
-      </div>
-      <div class="mx-2 text-center text-white">
-        <div class="countDown">
-          30
-        </div>
-        <p class="mt-1">
-          {{ $t('shared.time.sec') }}
-        </p>
-      </div>
-    </div>
+    <CountDown :timestamp="overview.deadline" />
 
     <div class="bg-white container mx-auto rounded-md overflow-hidden">
       <div class="px-4 py-3 border-b border-background flex flex-row justify-between items-center">
@@ -103,45 +70,80 @@
     </div>
 
     <div class="bg-white container mx-auto rounded-md overflow-hidden mt-4">
-      <p class="py-2 border-b border-background pl-4 font-medium">
+      <p class="py-4 pl-4 font-medium">
         {{ $t('spaceRace.ranks.title') }}
       </p>
-      <div class="flex flex-row items-center pt-3 pb-6 px-4 border-b border-background">
+      <div class="flex flex-row items-center pb-4 px-4 border-b border-background">
         <div v-for="item in continents" :key="item.code" class="rounded-full px-3 text-sm mr-3 border cursor-pointer hover:text-spaceRace hover:border-spaceRace" :class="{'text-spaceRace border-spaceRace':currentRankRegion === item.code}" @click="didRankRegionSwitched(item.code)">
           {{ item[$i18n.locale] }}
         </div>
       </div>
       <div class="px-4 text-sm my-2">
-        <table class="w-full">
-          <tr>
-            <th>
-              {{ $t('spaceRace.ranks.headers.rank') }}
-            </th>
-            <th>
-              {{ $t('spaceRace.ranks.headers.entity') }}
-            </th>
-            <th>
-              {{ $t('spaceRace.ranks.headers.area') }}
-            </th>
-            <th>
-              {{ $t('spaceRace.ranks.headers.minerCount') }}
-            </th>
-            <th>
-              {{ $t('spaceRace.ranks.headers.rawBytePower') }}
-            </th>
-            <th>
-              {{ $t('spaceRace.ranks.headers.estimatedTotalReward') }}
-            </th>
-            <th>
-              {{ $t('spaceRace.ranks.headers.estimatedRegionRewards') }}
-            </th>
-            <th>
-              {{ $t('spaceRace.ranks.headers.estimatedBlockRewards') }}
-            </th>
-          </tr>
+        <table v-loading="loading" class="w-full table-auto">
+          <thead class="text-gray-600">
+            <tr>
+              <th>
+                {{ $t('spaceRace.ranks.headers.rank') }}
+              </th>
+              <th>
+                {{ $t('spaceRace.ranks.headers.entity') }}
+              </th>
+              <th>
+                {{ $t('spaceRace.ranks.headers.area') }}
+              </th>
+              <th>
+                {{ $t('spaceRace.ranks.headers.minerCount') }}
+              </th>
+              <th>
+                {{ $t('spaceRace.ranks.headers.rawBytePower') }}
+              </th>
+              <th>
+                {{ $t('spaceRace.ranks.headers.estimatedTotalReward') }}
+              </th>
+              <th>
+                {{ $t('spaceRace.ranks.headers.estimatedRegionRewards') }}
+              </th>
+              <th>
+                {{ $t('spaceRace.ranks.headers.estimatedBlockRewards') }}
+              </th>
+              <th>
+              </th>
+            </tr>
+          </thead>
           <tbody>
-            <td>
-            </td>
+            <template v-for="(entity,index) in ranks.entities">
+              <tr :key="index" class="text-center border-b border-background h-10">
+                <td>
+                  {{ currentRankRegion === 'All' ? (entity.globalRank || '--') : (entity.regionRank || '--') }}
+                </td>
+                <td>
+                  {{ entity.name }}
+                </td>
+                <td>
+                  {{ getContinentNameByCode(entity.region) }}
+                </td>
+                <td>
+                  {{ entity.addresses.length }}
+                </td>
+                <td>
+                  {{ entity.power | size_metric(2) }}
+                </td>
+                <td>
+                  {{ entity.globalReward ? entity.globalReward.toFixed(2) : 'N/A' }} FIL
+                </td>
+                <td>
+                  {{ entity.regionReward ? entity.regionReward.toFixed(2) : 'N/A' }} FIL
+                </td>
+                <td>
+                  {{ entity.blockReward ? entity.blockReward.toFixed(2): 'N/A' }} FIL
+                </td>
+                <td>
+                  <div>
+                    <img src="~/assets/img/space-race/expand.svg" alt="expand" class="w-4">
+                  </div>
+                </td>
+              </tr>
+            </template>
           </tbody>
         </table>
       </div>
@@ -152,16 +154,28 @@
         {{ $t('spaceRace.distribution.title') }}
       </p>
       <SrMap map-width="100%" map-height="600px" />
+      <div class="grid grid-cols-3 gap-6 px-4 my-6 mx-4">
+        <div v-for="(r,index) in overview.regions" :key="index" class="border border-background rounded-sm hover:shadow text-sm px-4">
+          <p class=" font-medium py-2">
+            {{ getContinentNameByCode(r.id) }}
+          </p>
+          <div class="flex justify-between">
+            <span class="text-gray-600"> {{ $t('spaceRace.distribution.headers.unlockRewards') }}: </span>
+            <span> {{ getRewardsByPower(r.totalPower) }} </span>
+          </div>
+          <div class="flex justify-between py-2">
+            <span class="text-gray-600"> {{ $t('spaceRace.distribution.headers.rawBytePower') }}: </span>
+            <span> {{ r.totalPower | size_metric(2) }} </span>
+          </div>
+          <div class="flex justify-between pb-2">
+            <span class="text-gray-600"> {{ $t('spaceRace.distribution.headers.activeMiners') }}: </span>
+            <span> {{ r.activeMiners }} </span>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
-
-<style lang="postcss" scoped>
-.countDown {
-  @apply rounded w-10 h-10 text-xl font-bold justify-center flex items-center shadow;
-  background-image: linear-gradient(#0638AC,#012F9C)
-}
-</style>
 
 <script>
 import { continents } from '@/filecoin/continent'
@@ -181,9 +195,13 @@ export default {
   data() {
     return {
       overview: {},
-      region: 'All',
+      region: 'All', // for overview
       continents,
-      currentRankRegion: 'All'
+      currentRankRegion: 'All', // for ranks
+      ranks: {},
+      page: 0,
+      pageSize: 20,
+      loading: false
     }
   },
   computed: {
@@ -237,11 +255,43 @@ export default {
     }
   },
   mounted() {
-
+    this.getRanks()
   },
   methods: {
+    async getRanks() {
+      this.loading = true
+      const params = this.currentRankRegion === 'All' ? { pageSize: this.pageSize, page: this.page } : { pageSize: this.pageSize, page: this.page, region: this.currentRankRegion }
+      this.ranks = await this.$axios.$get(`/space-race/entities`, { params })
+      this.loading = false
+    },
     didRankRegionSwitched(code) {
       this.currentRankRegion = code
+      this.getRanks()
+    },
+    getRewardsByPower(power) {
+      const tib = 1099511627776
+      const pib = 1125899906842624
+      if (power - 10 * tib < 0) {
+        return '0 FIL'
+      } else if (power - 100 * tib < 0) {
+        return '25k FIL'
+      } else if (power - pib < 0) {
+        return '50k FIL'
+      } else if (power - 5 * pib < 0) {
+        return '100k FIL'
+      } else if (power - 10 * pib < 0) {
+        return '250k FIL'
+      } else {
+        return '500k FIL'
+      }
+    },
+    getContinentNameByCode(code) {
+      for (const continent of this.continents) {
+        if (code === continent.code) {
+          return continent[this.$i18n.locale]
+        }
+      }
+      return 'N/A'
     }
   },
   head() {
