02665190862630ddfd642751a352a4bf2043d3a1
m-stephen <truestyle2005@163.com>
2020-10-15 15:55:54 +0800

    fix bug

--- components/shared/navigation-bar-mobile.vue
+++ components/shared/navigation-bar-mobile.vue
@@ -62,9 +62,11 @@
         </el-collapse-item>
       </el-collapse>
 
-      <a href="https://wallet.filfox.info" target="_blank" class="block ranks text-sm py-2 cursor-pointer" @click.native="hideIfNeeded">
-        {{ $t('wallet.title') }}
-      </a>
+      <div class="block ranks text-sm py-2 cursor-pointer" @click="hideIfNeeded">
+        <a href="https://wallet.filfox.info" target="_blank">
+          {{ $t('wallet.title') }}
+        </a>
+      </div>
     </div>
     <div v-loading.fullscreen.lock="loading" class="flex flex-grow"></div>
   </div>
