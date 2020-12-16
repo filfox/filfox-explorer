3d8445fab3a026e779e722e7ec6dc02d2fcde669
Xuan Yan <xuanyan@xuanyan.ws>
2020-12-16 16:11:33 +0800

    forward backend request user info

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,5 +1,13 @@
-export default function({ $axios, app }) {
+export default function({ $axios, app, req }) {
   $axios.interceptors.request.use(config => {
+    if (process.server) {
+      if (req.headers['user-agent']) {
+        config.headers['User-Agent'] = req.headers['user-agent']
+      }
+      if (req.headers['x-forwarded-for']) {
+        config.headers['X-Forwarded-For'] = req.headers['x-forwarded-for']
+      }
+    }
     config.headers.Locale = app.i18n.locale
     return config
   })
