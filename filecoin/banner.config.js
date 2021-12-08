5ac126533bb2b36ed5fe43aa20901dbbd33fe849
Xingrui Ni <315631006@qq.com>
2021-12-08 10:04:16 +0800

    update banner

--- filecoin/banner.config.js
+++ filecoin/banner.config.js
@@ -1,21 +1,21 @@
 export const enBanners = [
   {
-    url: 'https://foxwallet.com/en',
+    url: 'https://t.me/FoxWallet_Official',
     img: require('~/assets/img/home/banner-en-1.jpg')
   },
   {
-    url: 'https://docs.google.com/forms/d/e/1FAIpQLSdWhNaQguufS88mHABbCEM78XF4OXzcYh-ycm6TV3QpTydyqQ/viewform?usp=send_form',
+    url: 'https://foxwallet.com/en',
     img: require('~/assets/img/home/banner-en-2.jpg')
   }
 ]
 
 export const zhBanners = [
   {
-    url: 'https://foxwallet.com/zh',
+    url: 'https://t.me/FoxWallet_Official',
     img: require('~/assets/img/home/banner-zh-1.jpg')
   },
   {
-    url: 'http://ab8p3tzb49ia4urb.mikecrm.com/mrTNcl2',
+    url: 'https://foxwallet.com/zh',
     img: require('~/assets/img/home/banner-zh-2.jpg')
   }
 ]
