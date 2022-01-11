04c260a9bff027a293050de52323682a4d1731b4
Xingrui Ni <315631006@qq.com>
2022-01-11 15:39:51 +0800

    remove Christmas banner

--- filecoin/banner.config.js
+++ filecoin/banner.config.js
@@ -1,19 +1,11 @@
 export const enBanners = [
   {
-    url: 'https://givelab.com/foxwalletxmas',
-    img: require('~/assets/img/home/banner-en-1.jpg')
-  },
-  {
     url: 'https://foxwallet.com/en',
     img: require('~/assets/img/home/banner-en-2.jpg')
   }
 ]
 
 export const zhBanners = [
-  {
-    url: 'https://giv.gg/foxwalletxmas',
-    img: require('~/assets/img/home/banner-zh-1.jpg')
-  },
   {
     url: 'https://foxwallet.com/zh',
     img: require('~/assets/img/home/banner-zh-2.jpg')
