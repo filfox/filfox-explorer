c68a4efaab8ae276560840e1d93671a10999d6d8
Xingrui Ni <315631006@qq.com>
2022-01-25 16:05:52 +0800

    FIX:update banner img

--- filecoin/banner.config.js
+++ filecoin/banner.config.js
@@ -1,13 +1,13 @@
 export const enBanners = [
   {
     url: 'https://foxwallet.com/en',
-    img: require('~/assets/img/home/banner-en-2.jpg')
+    img: require('~/assets/img/home/banner-en-1.jpg')
   }
 ]
 
 export const zhBanners = [
   {
     url: 'https://foxwallet.com/zh',
-    img: require('~/assets/img/home/banner-zh-2.jpg')
+    img: require('~/assets/img/home/banner-zh-1.jpg')
   }
 ]
