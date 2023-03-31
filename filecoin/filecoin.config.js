b0a0a5e1eb0dddfd8b72dae558d6626e4c96d9cf
wucjgit <470013468@qq.com>
2023-03-31 15:08:54 +0800

    Optimizing: configuration files

--- filecoin/filecoin.config.js
+++ filecoin/filecoin.config.js
@@ -1,23 +1,31 @@
 export const network = {
   // Index of networks
   multipleNetworks: true,
-  currentNetwork: 1,
+  currentNetwork: 2,
   networks: [
     {
       name: 'Mainnet',
-      url: 'https://filfox.info'
+      url: 'https://filfox.info',
+      fns: 'https://app.fns.space'
+    },
+    {
+      name: 'Calibration',
+      url: 'https://calibration.filfox.info',
+      fns: 'https://testapp.fns.space'
     },
     {
       name: 'Hyperspace',
-      url: 'https://hyperspace.filfox.info'
+      url: 'https://hyperspace.filfox.info',
+      fns: 'https://testapp.fns.space'
     }
   ]
 }
 
 export const wiki = {
-  url: 'http://localhost:3000/wiki'
+  url: 'https://filfox.info/wiki'
 }
 
-export const fnsServer = 'https://hyperspace.filfox.info/api/fns'
+export const FNS = network.networks[network.currentNetwork]
+export const fnsServer = `${FNS}/api/lookup`
 
 export const epochsInDay = 2880
