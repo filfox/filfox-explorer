60998eb3812f2850e787f6f3792a014322d4ebff
wucjgit <470013468@qq.com>
2023-04-03 15:09:31 +0800

    fix: bugs

--- nuxt.config.js
+++ nuxt.config.js
@@ -98,7 +98,7 @@ export default {
     'nuxt-i18n'
   ],
   axios: {
-    baseURL: 'https://hyperspace.filfox.info/api/v1'
+    baseURL: 'https://calibration.filfox.info/api/v1'
   },
   i18n: {
     locales: [
