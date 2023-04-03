2e228b7d0f33374e95329021c989b8ee8f472ccc
wucjgit <470013468@qq.com>
2023-04-03 13:35:57 +0800

    fix: home fns title

--- components/home/home-fns-registrations.vue
+++ components/home/home-fns-registrations.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <div class="bg-white rounded-md pb-2 mb-4 hidden lg:block">
-      <div class="flex justify-between items-center p-4">
+    <div class="bg-white rounded-md pb-2 mb-4">
+      <div class="flex justify-between items-center px-4 py-2 lg:py-4">
         <span class="text-sm lg:text-base flex items-center">
           <img src="../../assets/img/home/icon-fns.svg" alt="FNS" class="h-5 mr-2">
           {{ $t('fns.registrations.title') }}
@@ -13,47 +13,49 @@
           {{ $t('shared.more') }}
         </NuxtLink>
       </div>
-      <table class="w-full text-sm font-light border-t border-customGray-100">
-        <tr>
-          <th
-            v-for="({ key, label }, index) in columns"
-            :key="key"
-            :class="[index ? 'w-auto' : 'w-1/3']"
-            class="text-customGray-400 text-sm font-light pt-4 pb-1"
-          >
-            {{ label }}
-          </th>
-        </tr>
-        <tr v-for="{ owner, name, expires } in names" :key="name" class="text-center">
-          <td>
-            <AddressLink :id="owner" :format="18" class="text-main" />
-          </td>
-          <td>{{ name }}</td>
-          <td>{{ expires | timestamp('datetime') }}</td>
-          <td>
-            <NuxtLink
-              :to="localePath(`/fns/name/${name}`)"
-              class="px-4 py-1 rounded text-xs font-semibold text-main bg-customBlue-200 border border-transparent hover:border-main transition duration-200"
+      <div class="hidden lg:block">
+        <table class="w-full text-sm font-light border-t border-customGray-100">
+          <tr>
+            <th
+              v-for="({ key, label }, index) in columns"
+              :key="key"
+              :class="[index ? 'w-auto' : 'w-1/3']"
+              class="text-customGray-400 text-sm font-light pt-4 pb-1"
             >
-              {{ $t('shared.more') }}
-            </NuxtLink>
-          </td>
-        </tr>
-      </table>
+              {{ label }}
+            </th>
+          </tr>
+          <tr v-for="{ owner, name, expires } in names" :key="name" class="text-center">
+            <td>
+              <AddressLink :id="owner" :format="18" class="text-main" />
+            </td>
+            <td>{{ name }}</td>
+            <td>{{ expires | timestamp('datetime') }}</td>
+            <td>
+              <NuxtLink
+                :to="localePath(`/fns/name/${name}`)"
+                class="px-4 py-1 rounded text-xs font-semibold text-main bg-customBlue-200 border border-transparent hover:border-main transition duration-200"
+              >
+                {{ $t('shared.more') }}
+              </NuxtLink>
+            </td>
+          </tr>
+        </table>
+      </div>
+      <ul class="block lg:hidden bg-white p-3 text-xs font-light border-t border-customGray-100">
+        <li
+          v-for="item in names"
+          :key="item.name"
+          class="border-b border-customGray-100 py-2"
+        >
+          <div v-for="{ key, label } in columnsMobile" :key="key" class="flex justify-between items-center my-2">
+            <span class="text-customGray-400">{{ label }}</span>
+            <AddressLink v-if="key == 'owner'" :id="item[key]" :format="12" class="text-main" />
+            <span v-else class="w-2/3 break-all text-right">{{ item[key] | lookfortime('datetime') }}</span>
+          </div>
+        </li>
+      </ul>
     </div>
-    <ul class="block lg:hidden my-2 bg-white p-3 text-xs font-light">
-      <li
-        v-for="item in names"
-        :key="item.name"
-        class="border-b border-customGray-100 py-2"
-      >
-        <div v-for="{ key, label } in columnsMobile" :key="key" class="flex justify-between items-center my-2">
-          <span class="text-customGray-400">{{ label }}</span>
-          <AddressLink v-if="key == 'owner'" :id="item[key]" :format="12" class="text-main" />
-          <span v-else class="w-2/3 break-all text-right">{{ item[key] | lookfortime('datetime') }}</span>
-        </div>
-      </li>
-    </ul>
   </div>
 </template>
 
