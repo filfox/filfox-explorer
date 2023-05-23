9f035de8dabb646008f2970477da3e1f764d7e0d
wucjgit <470013468@qq.com>
2023-05-23 17:01:01 +0800

    add: stfil banner

--- filecoin/banner.config.js
+++ filecoin/banner.config.js
@@ -10,6 +10,11 @@ export const enBanners = [
     url: 'https://foxwallet.com/en?invite=evkZv8g5TG',
     img: require('~/assets/img/home/banner-en-1.png'),
     mobileImg: require('~/assets/img/home/banner-en-mobile-1.png')
+  },
+  {
+    url: 'https://stfil.io',
+    img: require('~/assets/img/home/banner-en-stfil.png'),
+    mobileImg: require('~/assets/img/home/banner-en-stfil-mobile.png')
   }
 ]
 
@@ -23,5 +28,11 @@ export const zhBanners = [
     url: 'https://foxwallet.com/zh?invite=evkZv8g5TG',
     img: require('~/assets/img/home/banner-zh-1.png'),
     mobileImg: require('~/assets/img/home/banner-zh-mobile-1.png')
+  },
+  {
+    url: 'https://stfil.io',
+    img: require('~/assets/img/home/banner-en-stfil.png'),
+    mobileImg: require('~/assets/img/home/banner-en-stfil-mobile.png')
   }
 ]
+
