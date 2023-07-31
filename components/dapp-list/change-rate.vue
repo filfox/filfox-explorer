e7ca3336321367671c637f2e14e431d5a9c55c03
LiaoLikun <liaolikun@web3.com>
2023-07-31 14:00:54 +0800

    ui change

--- components/dapp-list/change-rate.vue
+++ components/dapp-list/change-rate.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <span :class="`text-sm ${ changeRate > 0 || changeRate < 0? 'mr-2.5' : 'mr-0' }`">{{ data }}</span>
-    <span v-if="changeRate > 0" class="text-xs text-increaseNum">+{{ (Number(changeRate)*100).toFixed(2) }}%</span>
-    <span v-if="changeRate < 0" class="text-xs text-decreaseNum">{{ (Number(changeRate)*100).toFixed(2) }}%</span>
+  <div class="relative">
+    <span>{{ data }}</span>
+    <span v-if="changeRate > 0" class="text-xs absolute top-1px text-increaseNum whitespace-pre">  +{{ (Number(changeRate)*100).toFixed(2) }}%</span>
+    <span v-if="changeRate < 0" class="text-xs absolute top-1px text-decreaseNum whitespace-pre">  {{ (Number(changeRate)*100).toFixed(2) }}%</span>
   </div>
 </template>
 
